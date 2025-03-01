dcf29cef6b8beb95fda83aa18409a7fa0c0be99b
Igor Nechaev <i.nechaev@lipt-soft.ru>
2025-03-01 18:06:20 +0300

    [case-2](feat) initial solution

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -3,7 +3,11 @@ package ru.quipy.payments.logic
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -27,37 +31,71 @@ class OrderPayer {
     @Autowired
     private lateinit var paymentService: PaymentService
 
-//    {
-//        "serviceName": "onlineStore",
-//        "accountName": "acc-3",
-//        "parallelRequests": 30,
-//        "rateLimitPerSec": 10,
-//        "price": 30,
-//        "averageProcessingTime": "PT1S"
-//    }
+// PaymentAccountProperties(serviceName=onlineStore, accountName=acc-5, parallelRequests=5, rateLimitPerSec=3, price=30, averageProcessingTime=PT4.9S, enabled=false)
+// {
+//  "ratePerSecond": 2,
+//  "testCount": 100,
+//  "processingTimeMillis": 60000
+//}
+
+    /**
+     * 1) 2 -> payment (2)
+     * 2) 2 -> payment (4)
+     * 3) 2 -> payment (5) -> hold (1)
+     * 4) 2 -> payment (5) -> hold (3)
+     * 5) 2 -> payment (5) -> hold (5)
+     * 6) 2 -> payment (5) +2 -> hold (5) -2
+     * 7) 2 -> payment (5) +2 -> hold (5) -2
+     * 8) 2 -> payment (5) +2 -> hold (5) -2
+     * 9) 2 -> payment (5) +2 -> hold (3) -2
+     * 10) 2 -> payment (5) +2 -> hold (3) -2
+     *
+     *
+     * 1) window control - максимум 5 запросов в in flight
+     * 2) новые запросы сначала в hold
+     * 3) самые старые из hold в payment
+     */
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    private val semaphore = Semaphore(permits = 5)
+
+    private val counter = MutableStateFlow(0)
 
     private val slideWindow = SlidingWindowRateLimiter<Order>(
         rate = SERVICE_RATE_LIMIT_PER_SECOND,
         window = Duration.ofSeconds(1),
     )
 
-    private val checkDelayTime = 1.seconds.inWholeMilliseconds
-        .div(SERVICE_RATE_LIMIT_PER_SECOND).milliseconds.div(SPEED_CHECK_COFF)
+    private val checkDelayTime =
+        1.seconds.inWholeMilliseconds.div(SERVICE_RATE_LIMIT_PER_SECOND).milliseconds.div(SPEED_CHECK_COFF)
 
     fun processPayment(orderId: UUID, amount: Int, paymentId: UUID, deadline: Long): Long {
         val createdAt = System.currentTimeMillis()
+        val timeout = createdAt.plus(55100)
+        val order = Order(createdAt, paymentId)
+        val orderIndex = counter.getAndUpdate { oldCounter -> oldCounter.plus(1) }
         coroutineScope.launch {
-            val order = Order(createdAt, paymentId)
-            slideWindow.tickValueBlocking(order, checkDelayTime)
-            val createdEvent = paymentESService.create {
-                it.create(
-                    paymentId, orderId, amount
-                )
+            semaphore.withPermit {
+                slideWindow.tickValueBlocking(order, checkDelayTime)
+                val permittedAt = System.currentTimeMillis()
+                when {
+                    permittedAt < timeout -> {
+                        logger.error("ORDER index: $orderIndex")
+                        val createdEvent = paymentESService.create {
+                            it.create(
+                                paymentId, orderId, amount
+                            )
+                        }
+                        logger.trace("Payment {} for order {} created.", createdEvent.paymentId, orderId)
+                        paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
+                    }
+
+                    else -> {
+                        logger.error("TIMEEOUT ORDER index: $orderIndex")
+                    }
+                }
             }
-            logger.trace("Payment {} for order {} created.", createdEvent.paymentId, orderId)
-            paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
         }
         return createdAt
     }
@@ -69,7 +107,7 @@ class OrderPayer {
 
     private companion object {
 
-        const val SERVICE_RATE_LIMIT_PER_SECOND = 10L
+        const val SERVICE_RATE_LIMIT_PER_SECOND = 3L
 
         const val SPEED_CHECK_COFF = 1
     }
--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -2,6 +2,7 @@ package ru.quipy.payments.logic
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
@@ -11,6 +12,8 @@ import ru.quipy.payments.api.PaymentAggregate
 import java.net.SocketTimeoutException
 import java.time.Duration
 import java.util.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 
 // Advice: always treat time as a Duration
@@ -34,7 +37,7 @@ class PaymentExternalSystemAdapterImpl(
 
     private val client = OkHttpClient.Builder().build()
 
-    override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
+    override suspend fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
         logger.warn("[$accountName] Submitting payment request for payment $paymentId")
 
         val transactionId = UUID.randomUUID()
@@ -51,39 +54,43 @@ class PaymentExternalSystemAdapterImpl(
             post(emptyBody)
         }.build()
 
-        try {
-            client.newCall(request).execute().use { response ->
-                val body = try {
-                    mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
-                } catch (e: Exception) {
-                    logger.error("[$accountName] [ERROR] Payment processed for txId: $transactionId, payment: $paymentId, result code: ${response.code}, reason: ${response.body?.string()}")
-                    ExternalSysResponse(transactionId.toString(), paymentId.toString(),false, e.message)
-                }
+        suspendCancellableCoroutine { continuation ->
+            try {
+                client.newCall(request).execute().use { response ->
+                    val body = try {
+                        mapper.readValue(response.body?.string(), ExternalSysResponse::class.java)
+                    } catch (e: Exception) {
+                        logger.error("[$accountName] [ERROR] Payment processed for txId: $transactionId, payment: $paymentId, result code: ${response.code}, reason: ${response.body?.string()}")
+                        ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
+                    }
 
-                logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
+                    logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
 
-                // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
-                // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
-                paymentESService.update(paymentId) {
-                    it.logProcessing(body.result, now(), transactionId, reason = body.message)
-                }
-            }
-        } catch (e: Exception) {
-            when (e) {
-                is SocketTimeoutException -> {
-                    logger.error("[$accountName] Payment timeout for txId: $transactionId, payment: $paymentId", e)
+                    // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
+                    // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
                     paymentESService.update(paymentId) {
-                        it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
+                        it.logProcessing(body.result, now(), transactionId, reason = body.message)
+                    }
+                    continuation.resume(Unit)
+                }
+            } catch (e: Exception) {
+                when (e) {
+                    is SocketTimeoutException -> {
+                        logger.error("[$accountName] Payment timeout for txId: $transactionId, payment: $paymentId", e)
+                        paymentESService.update(paymentId) {
+                            it.logProcessing(false, now(), transactionId, reason = "Request timeout.")
+                        }
+                    }
+
+                    else -> {
+                        logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
+
+                        paymentESService.update(paymentId) {
+                            it.logProcessing(false, now(), transactionId, reason = e.message)
+                        }
                     }
                 }
-
-                else -> {
-                    logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
-
-                    paymentESService.update(paymentId) {
-                        it.logProcessing(false, now(), transactionId, reason = e.message)
-                    }
-                }
+                continuation.resumeWithException(e)
             }
         }
     }
